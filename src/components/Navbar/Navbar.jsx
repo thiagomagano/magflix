8c87e270ca0ce573cb553d39749e2ea1d0d9b810
Thiago Magano <thiagomagano1993@outlook.com>
2020-07-27 14:17:39 -0300

    fix logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import './Navbar.css'
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
 
 function Navbar() {
-    const [show, handleShow] = useState(false);
+  const [show, handleShow] = useState(false);
 
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 110) {
-                handleShow(true);
-            } else handleShow(false);
-        });
-        return () => {
-            window.removeEventListener('scroll')
-        }
-    }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 110) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
 
+  return (
+    <div className={`nav ${show && "nav-black"}`}>
+      <span className="logo">
+        <img
+          className="logo-img"
+          src="https://fontmeme.com/permalink/200727/00f5cf7e36d644b4b537c3a7e8cae24b.png"
+          alt="logo-magflix"
+        />
+      </span>
 
-    return (
-        <div className={`nav ${show && "nav-black"}`}>
-
-            <span className="logo">MAGFLIX</span>
-
-            <img
-                src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png"
-                alt="User Avatar"
-                className="nav-avatar"
-            />
-
-        </div>
-    )
+      <img
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png"
+        alt="User Avatar"
+        className="nav-avatar"
+      />
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
